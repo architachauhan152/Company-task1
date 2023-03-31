7ca22b1721e50656957975d189f854a2248b9f17
Ritu Chauhan <93373957+architachauhan152@users.noreply.github.com>
2023-03-31 17:43:54 +0530

    done

---
+++ navadhiti/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SupportForm from './Components/SupportForm/SupportForm';
+
+function App() {
+  return (
+    <div className="App">
+      <SupportForm />
+    </div>
+  );
+}
+
+export default App;
